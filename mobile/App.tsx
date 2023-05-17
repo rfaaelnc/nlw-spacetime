4eb672f3a410a860e9404df1e298572c2b34e639
rfaaelnc <rfaaelnc@gmail.com>
2023-05-17 15:52:44 -0300

    mobile: added colors + fonts configuration

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 // dp
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts) return null
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-5xl font-bold text-zinc-50">Rocketseat</Text>
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text className="font-alt text-5xl text-zinc-50">Rocketseat</Text>
       <StatusBar style="light" translucent />
     </View>
   )
