62d40b43626ccf86c1bc9d91029c5f903844ae44
rfaaelnc <rfaaelnc@gmail.com>
2023-05-17 23:25:52 -0300

    server: memory crud added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,10 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 // HTTP Method: GET, POST, PUT, PATCH, DELETE
-
-app.get('/hello', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
-})
+app.register(memoriesRoutes)
 
 app
   .listen({
