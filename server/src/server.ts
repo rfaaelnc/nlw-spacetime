f432ba6c941df8a26f31d34bb887ef1a1e13b830
rfaaelnc <rfaaelnc@gmail.com>
2023-05-17 23:28:32 -0300

    server: cors added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
-// HTTP Method: GET, POST, PUT, PATCH, DELETE
+app.register(cors, {
+  origin: true,
+})
+
 app.register(memoriesRoutes)
 
 app
